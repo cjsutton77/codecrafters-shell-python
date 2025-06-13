b6c97ebbd3b976af9856e889d85ae5d2d500ef9e
Christian Sutton <christian.sutton@gmail.com>
2025-06-13 18:40:20 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,15 +6,24 @@ def main():
         while (True):
             # Uncomment this block to pass the first stage
             sys.stdout.write("$ ")
-
             # Wait for user input
             command = input()
-            if (command == EXIT_STRING):
-                break
             commArr = command.split(' ')
-            if (commArr[0] == 'echo'):
+            if (command == EXIT_STRING):
+                sys.exit(0)
+            elif (commArr[0] == 'echo'):
                 commArr.pop(0)
                 print(f'{' '.join(commArr)}')
+            elif (commArr[0] == 'type'):
+                match commArr[1]:
+                    case "echo":
+                        print(f'{commArr[1]} is a shell builtin')
+                    case "exit":
+                        print(f'{commArr[1]} is a shell builtin')
+                    case "type":
+                        print(f'{commArr[1]} is a shell builtin')    
+                    case _:
+                        print(f'{commArr[1]}: not found') 
             else:
                 print(f"{command}: command not found")
 
